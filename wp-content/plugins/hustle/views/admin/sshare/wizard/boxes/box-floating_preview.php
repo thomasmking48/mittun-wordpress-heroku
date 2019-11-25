67002aaf4c891c7d8c6d48d9a1361c15128e892f
Tom King <thomasmking48@gmail.com>
2019-11-25 13:48:50 -0500

    re-added default theme

--- wp-content/plugins/hustle/views/admin/sshare/wizard/boxes/box-floating_preview.php
+++
@@ -1,7 +0,0 @@
-<label><?php esc_attr_e( "Module preview", Opt_In::TEXT_DOMAIN ); ?></label>
-
-<div id="wph-sshare-preview-floating">
-
-    <?php $this->render( "general/modals/shares-floating", array( 'is_preview' => true ) ); ?>
-
-</div>
--- wp-content/plugins/salient-core/includes/vc_templates/toggle.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-extract(shortcode_atts(array(
-	"title" => 'Title', 
-	'color' => 'Accent-Color'), $atts));  
-
-echo '<div class="toggle '.esc_attr(strtolower($color)).'"><h3><a href="#"><i class="icon-plus-sign"></i>'. wp_kses_post($title) .'</a></h3><div>' . do_shortcode($content) . '</div></div>';
-	
-?>
