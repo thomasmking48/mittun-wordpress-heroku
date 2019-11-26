094976efb53f8adc77dcb44fedd2f24a310b156c
Tom King <thomasmking48@gmail.com>
2019-11-25 19:12:36 -0500

    New Heroku files

--- wp-content/plugins/hustle/inc/providers/aweber/aweber.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Direct Load
-require_once dirname( __FILE__ ) . '/hustle-aweber.php';
-require_once dirname( __FILE__ ) . '/hustle-aweber-form-settings.php';
-Hustle_Providers::get_instance()->register( 'Hustle_Aweber' );
